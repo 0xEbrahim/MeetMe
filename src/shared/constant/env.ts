2bb1ecb415773c70de9aef66d5aa3304d5be4f87
Ebrahim <elsayedebrahim603@gmail.com>
2025-06-10 18:55:56 +0300

    feat: update server configuration and Docker setup - Modified docker-compose.yml, server.ts, and env.ts

--- src/shared/constant/env.ts
+++ src/shared/constant/env.ts
@@ -8,10 +8,10 @@ export default {
   NODE_ENV: env.get("NODE_ENV").required().asString(),
   PORT: env.get("PORT").required().asPortNumber(),
   MONGO_USERNAME: env.get("MONGO_INITDB_ROOT_USERNAME").required().asString(),
-  MONGO__PASSWORD: env
-    .get("ME_CONFIG_MONGODB_ADMINPASSWORD")
-    .required()
-    .asString(),
+  MONGO_PASSWORD: env.get("MONGO_INITDB_ROOT_PASSWORD").required().asString(),
+  MONGO_DB_NAME: env.get("MONGO_INITDB_DATABASE").required().asString(),
+  MONGO_DB_HOST: env.get("MONGO_DB_HOST").required().asString(),
+  MONGO_DB_PORT: env.get("MONGO_DB_PORT").required().asPortNumber(),
   ME_USERNAME: env.get("ME_CONFIG_MONGODB_ADMINUSERNAME").required().asString(),
   ME_PASSWORD: env.get("ME_CONFIG_MONGODB_ADMINPASSWORD").required().asString(),
   ME_URI: env.get("ME_CONFIG_MONGODB_URL").required().asString(),
