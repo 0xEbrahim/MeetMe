33343f1c3fc48687a706644d5091dcaf52413739
Ebrahim <elsayedebrahim603@gmail.com>
2025-06-10 20:07:10 +0300

    feat add AppError class

--- src/infrastructure/api/server.ts
+++ src/infrastructure/api/server.ts
@@ -1,6 +1,6 @@
 import app from "./app";
-import { dbConnection } from "./infrastructure/database/connection";
-import env from "./shared/constant/env";
+import { dbConnection } from "../database/connection";
+import env from "../../shared/constant/env";
 
 app.listen(env.PORT, () => {
   console.log(`Server started successfully at port ${env.PORT}.`);
